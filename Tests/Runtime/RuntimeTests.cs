9086f4ceffce80d605ca1487321c3b3e9e10a71e
sindharta-tanuwijaya <71803280+sindharta-tanuwijaya@users.noreply.github.com>
2020-09-29 18:27:14 +0900

    fix: fix CI requirements (#7)
    * Add additional Unity versions to be tested in CI 
    * Fix mac build image in CI
    * Rename package to 0.0.5-preview
    * Add initial tests
    * Upgrade project version of the test project
    * Add symbolic links for the test project
    * Add packages-lock.json
    * Ignore log files

---
+++ Tests/Runtime/RuntimeTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Unity.SelectionGroups.Tests {
+public class RuntimeTests
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void RuntimeTestsSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator RuntimeTestsWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
+
+} //end namespace
+
