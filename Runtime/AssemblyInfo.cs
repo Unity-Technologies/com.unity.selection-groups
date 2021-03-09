60dfe565b4a453a577718e4709c438b09b5c2d00
sindharta <sindharta@gmail.com>
2021-03-09 23:17:26 +0900

    Add AssemblyInfo to open internals

---
+++ Runtime/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Unity.MaterialSwitch")]
+[assembly: InternalsVisibleTo("Unity.MaterialSwitch.Tests")]
+[assembly: InternalsVisibleTo("Unity.MaterialSwitch.Editor")]
+[assembly: InternalsVisibleTo("Unity.MaterialSwitch.EditorTests")]
+
+[assembly: InternalsVisibleTo("Unity.SelectionGroups")]
+[assembly: InternalsVisibleTo("Unity.SelectionGroups.Tests")]
+[assembly: InternalsVisibleTo("Unity.SelectionGroups.Editor")]
+[assembly: InternalsVisibleTo("Unity.SelectionGroups.EditorTests")]
+
+[assembly: InternalsVisibleTo("Unity.VisualCompositor")]
+[assembly: InternalsVisibleTo("Unity.VisualCompositor.Tests")]
+[assembly: InternalsVisibleTo("Unity.VisualCompositor.Editor")]
+[assembly: InternalsVisibleTo("Unity.VisualCompositor.EditorTests")]
+
+
