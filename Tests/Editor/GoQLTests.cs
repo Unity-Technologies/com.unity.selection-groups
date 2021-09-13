7a55e16f145b6f6e5215d6c447bb682a74a93496
Simon Wittber <simonwittber@unity3d.com>
2021-09-13 09:59:41 +0800

    initial goql tests

---
+++ Tests/Editor/GoQLTests.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using Unity.GoQL;
+using UnityEngine;
+
+namespace Tests
+{
+    public class GoQLTests
+    {
+        [Test]
+        public void TestTokenizerSimple()
+        {
+            var tokenizer = new Tokenizer();
+            var tokens = tokenizer.Tokenize("simon,wittber,123");
+            Debug.Log(string.Join(" | ", tokens));
+            Assert.AreEqual(TokenType.String, tokens[0].type);
+            Assert.AreEqual(TokenType.Comma, tokens[1].type);
+            Assert.AreEqual(TokenType.String, tokens[2].type);
+            Assert.AreEqual(TokenType.Comma, tokens[3].type);
+            Assert.AreEqual(TokenType.Number, tokens[4].type);
+        }
+
+        [Test]
+        public void TestTokenizerNotSoSimple()
+        {
+            var tokenizer = new Tokenizer();
+            var tokens = tokenizer.Tokenize("/simon*[wittber],-123, 3.14, <:> **");
+            Debug.Log(string.Join(" | ", tokens));
+            Assert.AreEqual(TokenType.Slash, tokens[0].type);
+            Assert.AreEqual(TokenType.String, tokens[1].type);
+            Assert.AreEqual(TokenType.OpenSquare, tokens[2].type);
+            Assert.AreEqual(TokenType.String, tokens[3].type);
+            Assert.AreEqual(TokenType.CloseSquare, tokens[4].type);
+            Assert.AreEqual(TokenType.Comma, tokens[5].type);
+            Assert.AreEqual(TokenType.Number, tokens[6].type);
+            Assert.AreEqual(TokenType.Comma, tokens[7].type);
+            Assert.AreEqual(TokenType.Number, tokens[8].type);
+            Assert.AreEqual(TokenType.Comma, tokens[9].type);
+            Assert.AreEqual(TokenType.OpenAngle, tokens[10].type);
+            Assert.AreEqual(TokenType.Colon, tokens[11].type);
+            Assert.AreEqual(TokenType.CloseAngle, tokens[12].type);
+            Assert.AreEqual(TokenType.Operator, tokens[13].type);
+            
+        }
+    }
+}
