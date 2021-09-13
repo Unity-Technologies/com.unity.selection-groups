33956aa540c64320fc7e232cb7c058daff7a24c7
Simon Wittber <simonwittber@unity3d.com>
2021-09-13 09:59:41 +0800

    initial goql tests

--- Tests/Editor/EditorTests.cs
+++ Tests/Editor/EditorTests.cs
@@ -1,28 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
+using Unity.GoQL;
 using UnityEngine;
 using UnityEngine.TestTools;
 
+
 namespace Tests
 {
-public class EditorTests
-{
-    // A Test behaves as an ordinary method
-    [Test]
-    public void EditorTestsSimplePasses()
+    public class EditorTests
     {
-        // Use the Assert class to test conditions
-    }
+        public string[] test_queries = new[]
+        {
+            "Head",
+            "*Head",
+            "Head*",
+            "*Head*",
+            "/Head",
+            "Head/",
+            "Head/[0]",
+            "Head/[0,1,5]",   
+            "Head/[-1]",
+            "Head/[3:5]",
+            "Head<t:Collider>",
+            "Head<m:Glow>",
+            "Head<s:Standard>",
+            "/",
+            "Quad*/<t:AudioSource>[1]", 
+            "<t:Transform, t:AudioSource>",    
+            "<t:Renderer>/*Audio*/[0:3]",
+            "Cube/Quad/<t:AudioSource>[-1]", 
+            "<m:Skin>",
+            "/Environment/**<t:MeshRenderer>", 
+        };
+        
+        
+        [Test]
+        public void TestGoQLExamples()
+        {
+            for (var i = 0; i < test_queries.Length; i++)
+            {
+                var q = test_queries[i];
+                var e = new GoQLExecutor();
+                e.Code = q;
+                var results = e.Execute();
+                Assert.AreEqual(ParseResult.OK, e.parseResult);
+            }
+        }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator EditorTestsWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        
     }
-}
 } //end namespace
