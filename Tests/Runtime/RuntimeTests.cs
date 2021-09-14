6721ffbf4544fdc3523ae0402a6f4ab5c6f69ab2
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-14 18:37:24 +0900

    update RuntimeTests

--- Tests/Runtime/RuntimeTests.cs
+++ Tests/Runtime/RuntimeTests.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Unity.SelectionGroupsEditor.Tests {
+namespace Unity.SelectionGroups.Tests {
 public class RuntimeTests
 {
     // A Test behaves as an ordinary method
