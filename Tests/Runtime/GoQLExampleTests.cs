931e046990f095f6462e853112cf117c59763215
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-21 14:24:37 +0900

    Remove Duplicate code

--- Tests/Runtime/GoQLExampleTests.cs
+++ Tests/Runtime/GoQLExampleTests.cs
@@ -27,69 +27,6 @@ namespace Unity.SelectionGroups.Tests
             yield return EditorSceneManager.LoadSceneAsyncInPlayMode($"{SelectionGroupsTestsConstants.EmptyScenePath}.unity", 
                 new LoadSceneParameters(LoadSceneMode.Single));
         }
-
-        [Test]
-        public void TestGoQLExamples5()
-        {
-            var e = new GoQLExecutor("/Head");
-            var results = e.Execute();
-            Assert.AreEqual(ParseResult.OK, e.parseResult);
-            Assert.AreEqual(1, results.Length);
-        }
-        
-        [Test]
-        public void TestGoQLExamples6()
-        {
-            var e = new GoQLExecutor("Head/");
-            var results = e.Execute();
-            Assert.AreEqual(ParseResult.OK, e.parseResult);
-            Assert.AreEqual(11, results.Length);
-        }
-        
-        [Test]
-        public void TestGoQLExamples7()
-        {
-            var e = new GoQLExecutor("Head/[0]");
-            var results = e.Execute();
-            Assert.AreEqual(ParseResult.OK, e.parseResult);
-            Assert.AreEqual(2, results.Length);
-            Assert.AreEqual("Child (0)", results[0].name);
-        }
-        
-        [Test]
-        public void TestGoQLExamples8()
-        {
-            var e = new GoQLExecutor("Head/[0,1,5]");
-            var results = e.Execute();
-            Assert.AreEqual(ParseResult.OK, e.parseResult);
-            Assert.AreEqual(5, results.Length);
-            // Assert.AreEqual("Child (0)", results[0].name);
-            // Assert.AreEqual("Child (1)", results[1].name);
-            // Assert.AreEqual("Child (5)", results[2].name);
-        }
-        
-        [Test]
-        public void TestGoQLExamples9()
-        {
-            var e = new GoQLExecutor("Head/[-1]");
-            var results = e.Execute();
-            Assert.AreEqual(ParseResult.OK, e.parseResult);
-            Assert.AreEqual(1, results.Length);
-            Assert.AreEqual("Head", results[0].name);
-        }
-        
-        [Test]
-        public void TestGoQLExamples10()
-        {
-            var e = new GoQLExecutor("Head/[3:5]");
-            var results = e.Execute();
-            Assert.AreEqual(ParseResult.OK, e.parseResult);
-            Assert.AreEqual(4, results.Length);
-            Assert.AreEqual("Child (3)", results[0].name);
-            Assert.AreEqual("Quad 1", results[1].name);
-            Assert.AreEqual("Child (4)", results[2].name);
-            Assert.AreEqual("Quad 2", results[3].name);
-        }
         
         [Test]
         public void TestGoQLExamples11()
