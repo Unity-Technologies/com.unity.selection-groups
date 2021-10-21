a44f2d6614038aeef1b2cfce2409adfecb9360b3
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-21 18:03:29 +0900

    Add TestUtility

--- Tests/Runtime/TestUtility.cs
+++ Tests/Runtime/TestUtility.cs
@@ -1,4 +1,6 @@
-﻿using Unity.GoQL;
+﻿using System;
+using System.Collections.Generic;
+using Unity.GoQL;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -13,8 +15,25 @@ internal static class TestUtility
         Assert.AreEqual(ParseResult.OK, e.parseResult);
         Assert.AreEqual(numExpectedResults, results.Length);
         return results;
-
     }
+    
+    internal static List<Transform> ExecuteGoQLAndVerify(string goql, int numExpectedResults, Func<Transform, bool> validFunc) {
+        GoQLExecutor e       = new GoQLExecutor(goql);
+        GameObject[] results = e.Execute();
+
+        Assert.AreEqual(ParseResult.OK, e.parseResult);
+        List<Transform> ret = new List<Transform>(results.Length);
+        foreach (GameObject go in results) {
+            Transform t = go.transform;
+            Assert.IsTrue(validFunc(t));
+            ret.Add(t);
+
+        }
+
+        return ret;
+            
+    }
+    
     
 
 
