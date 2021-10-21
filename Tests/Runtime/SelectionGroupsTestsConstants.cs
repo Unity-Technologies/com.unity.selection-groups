6f19d66ae1c231ae1de378809ad20394e5f957dc
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-21 15:49:33 +0900

    Remove constant

--- Tests/Runtime/SelectionGroupsTestsConstants.cs
+++ Tests/Runtime/SelectionGroupsTestsConstants.cs
@@ -5,7 +5,6 @@ namespace Unity.SelectionGroups.Tests {
 internal class SelectionGroupsTestsConstants
 {
     internal static string TestScenePath  = "Packages/com.unity.selection-groups/Tests/Scenes/GoQLTestScene";
-    internal static string EmptyScenePath = "Packages/com.unity.selection-groups/Tests/Scenes/EmptyScene";
     
 
 
