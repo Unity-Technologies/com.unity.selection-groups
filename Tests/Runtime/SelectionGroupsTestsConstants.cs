9a69d601ac725f77677ad99fe37a41e879a63ad7
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-21 14:13:58 +0900

    Add SelectionGroupsTestConstants

---
+++ Tests/Runtime/SelectionGroupsTestsConstants.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Unity.SelectionGroups.Tests {
+
+internal class SelectionGroupsTestsConstants
+{
+    private static string TestScenePath = "Packages/com.unity.selection-groups/Tests/Scenes/GoQLTestScene";
+    private static string EmptyScenePath = "Packages/com.unity.selection-groups/Tests/Scenes/EmptyScene";
+    
+
+
+}
+
+} //end namespace
