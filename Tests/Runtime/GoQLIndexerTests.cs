c1834b3f8bf11cdee507de0879b5f9b763ab6cb5
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-10-21 15:45:32 +0900

    use TestUtility in GoQLIndexer

--- Tests/Runtime/GoQLIndexerTests.cs
+++ Tests/Runtime/GoQLIndexerTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using Unity.GoQL;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
@@ -12,30 +13,22 @@ internal class GoQLIndexerTests
 {
     
     [UnitySetUp]
-    public IEnumerator SetUp()
-    {
+    public IEnumerator SetUp() {
         Assert.IsTrue(System.IO.File.Exists($"{SelectionGroupsTestsConstants.TestScenePath}.unity"));
         yield return EditorSceneManager.LoadSceneAsyncInPlayMode($"{SelectionGroupsTestsConstants.TestScenePath}.unity", 
             new LoadSceneParameters(LoadSceneMode.Single));
     }
     
     [Test]
-    public void FirstChild()
-    {
-        var e = new GoQLExecutor("Head/[0]");
-        var results = e.Execute();
-        Assert.AreEqual(ParseResult.OK, e.parseResult);
-        Assert.AreEqual(2, results.Length);
+    public void FirstChild() {
+        GameObject[] results = TestUtility.ExecuteGoQLAndVerify("Head/[0]", 2);
         Assert.AreEqual("Child (0)", results[0].name);
     }
     
     [Test]
     public void IndexedChildren()
     {
-        var e = new GoQLExecutor("Head/[0,1,5]");
-        var results = e.Execute();
-        Assert.AreEqual(ParseResult.OK, e.parseResult);
-        Assert.AreEqual(5, results.Length);
+        GameObject[] results = TestUtility.ExecuteGoQLAndVerify("Head/[0,1,5]", 5);
         // Assert.AreEqual("Child (0)", results[0].name);
         // Assert.AreEqual("Child (1)", results[1].name);
         // Assert.AreEqual("Child (5)", results[2].name);
@@ -44,20 +37,14 @@ internal class GoQLIndexerTests
     [Test]
     public void LastChild()
     {
-        var e = new GoQLExecutor("Head/[-1]");
-        var results = e.Execute();
-        Assert.AreEqual(ParseResult.OK, e.parseResult);
-        Assert.AreEqual(1, results.Length);
+        GameObject[] results = TestUtility.ExecuteGoQLAndVerify("Head/[-1]", 1);
         Assert.AreEqual("Head", results[0].name);
     }
     
     [Test]
     public void RangedChildren()
     {
-        var e = new GoQLExecutor("Head/[3:5]");
-        var results = e.Execute();
-        Assert.AreEqual(ParseResult.OK, e.parseResult);
-        Assert.AreEqual(4, results.Length);
+        GameObject[] results = TestUtility.ExecuteGoQLAndVerify("Head/[3:5]", 4);
         Assert.AreEqual("Child (3)", results[0].name);
         Assert.AreEqual("Quad 1", results[1].name);
         Assert.AreEqual("Child (4)", results[2].name);
