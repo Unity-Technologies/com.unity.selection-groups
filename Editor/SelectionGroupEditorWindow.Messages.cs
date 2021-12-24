88abdb4eb68dbbf544f650c6ddb5bddaf50128c2
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2021-12-24 18:55:23 +0900

    remove: command handling

--- Editor/SelectionGroupEditorWindow.Messages.cs
+++ Editor/SelectionGroupEditorWindow.Messages.cs
@@ -39,16 +39,6 @@ namespace Unity.SelectionGroupsEditor
 
                 SetupStyles();
                 DrawGUI();
-
-                switch (Event.current.type)
-                {
-                    case EventType.ValidateCommand:
-                        OnValidateCommand(Event.current);
-                        break;
-                    case EventType.ExecuteCommand:
-                        OnExecuteCommand(Event.current);
-                        break;
-                }
             }
             finally
             {
@@ -58,38 +48,6 @@ namespace Unity.SelectionGroupsEditor
             
         }
 
-        void OnExecuteCommand(Event current)
-        {
-            if (activeSelectionGroup != null)
-                switch (current.commandName)
-                {
-                    case "SelectAll":
-                        Selection.objects = activeSelectionGroup.Members.ToArray();
-                        UpdateActiveSelection();
-                        current.Use();
-                        break;
-                    case "DeselectAll":
-                        Selection.objects = null;
-                        UpdateActiveSelection();
-                        current.Use();
-                        break;
-                    case "InvertSelection":
-                        Selection.objects = new HashSet<Object>(activeSelectionGroup.Members).Except(Selection.objects).ToArray();
-                        UpdateActiveSelection();
-                        current.Use();
-                        break;
-                    case "SoftDelete":
-                        if (!isReadOnly)
-                        {
-                            activeSelectionGroup.Remove(Selection.objects);
-                            Selection.objects = null;
-                            UpdateActiveSelection();
-                            current.Use();
-                        }
-                        return;
-                }
-        }
-        
         void OnSelectionChange()
         {
             UpdateActiveSelection();
@@ -102,24 +60,5 @@ namespace Unity.SelectionGroupsEditor
                 activeSelection.UnionWith(Selection.objects);
         }
 
-        void OnValidateCommand(Event current)
-        {
-            switch (current.commandName)
-            {
-                case "SelectAll":
-                    current.Use();
-                    return;
-                case "DeselectAll":
-                    current.Use();
-                    return;
-                case "InvertSelection":
-                    current.Use();
-                    return;
-                case "SoftDelete":
-                    if (activeSelectionGroup != null)
-                        current.Use();
-                    return;
-            }
-        }
     }
 }
