c0f2eac04e2b819132f2cf52abcd819e7d337183
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-01-05 22:46:07 +0900

    refactor

--- Runtime/Scripts/GroupMembersSelection.cs
+++ Runtime/Scripts/GroupMembersSelection.cs
@@ -27,22 +27,35 @@ internal class GroupMembersSelection : IEnumerable<KeyValuePair<ISelectionGroup,
         m_selectedGroupMembers[group].Add(member);
     }
 
+    internal void AddGroupMembers(ISelectionGroup group) {
+        AddObjectsToSelection(group, group.Members);
+    }
+    
+    
     internal void Add(GroupMembersSelection otherSelection) {
         foreach (KeyValuePair<ISelectionGroup, OrderedSet<Object>> kv in otherSelection) {
-            ISelectionGroup    group             = kv.Key;
-            OrderedSet<Object> membersCollection = null;
-            if (!m_selectedGroupMembers.ContainsKey(group)) {
-                membersCollection = new OrderedSet<Object>() { };
-                m_selectedGroupMembers.Add(group, membersCollection);
-            } else {
-                membersCollection = m_selectedGroupMembers[group];
-            }
-
-            foreach (Object m in kv.Value) {
-                membersCollection.Add(m);
-            }
+            ISelectionGroup group = kv.Key;
+            AddObjectsToSelection(group, kv.Value);
         }
     }
+    
+
+    private void AddObjectsToSelection(ISelectionGroup group, IEnumerable<Object> objects) {
+        
+        OrderedSet<Object> collection = null;
+        if (!m_selectedGroupMembers.ContainsKey(group)) {
+            collection = new OrderedSet<Object>() { };
+            m_selectedGroupMembers.Add(group, collection);
+        } else {
+            collection = m_selectedGroupMembers[group];
+        }
+
+        foreach (Object m in objects) {
+            collection.Add(m);
+        }
+        
+    }
+    
 
     internal void Remove(ISelectionGroup group, Object member) {
         if (!m_selectedGroupMembers.ContainsKey(group)) {
