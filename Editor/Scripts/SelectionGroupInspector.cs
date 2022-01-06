02a965f76765f6d93bf6ffcf18cc7ad6b7b1796a
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-01-06 10:57:11 +0900

    Moving ConfigurationDialog to inspector

--- Editor/Scripts/SelectionGroupInspector.cs
+++ Editor/Scripts/SelectionGroupInspector.cs
@@ -1,5 +1,8 @@
+using System.Reflection;
+using Unity.SelectionGroups;
 using Unity.SelectionGroups.Runtime;
 using UnityEditor;
+using UnityEngine;
 
 namespace Unity.SelectionGroupsEditor {
 
@@ -13,10 +16,132 @@ internal class SelectionGroupInspector : Editor {
         base.OnInspectorGUI();
         if(group.Query != query)
             SelectionGroupManager.ExecuteQuery(group);
+        
+        OnGUI();
         //[TODO-sin:2021-12-20] Remove in version 0.7.0 
         // if(group.Scope != scope) 
         //     SelectionGroupManager.ChangeGroupScope(group, group.Scope);
     }
+    
+//----------------------------------------------------------------------------------------------------------------------    
+    private void OnEnable() {
+        m_group = target as ISelectionGroup;
+        Undo.undoRedoPerformed -= OnUndoRedo;
+        Undo.undoRedoPerformed += OnUndoRedo;
+    }
+
+    private void OnUndoRedo()
+    {
+        refreshQuery = true;
+        Repaint();
+    }
+
+    private void OnDisable() {
+        Undo.undoRedoPerformed -= OnUndoRedo;
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    void OnGUI()
+    {
+        using (var cc = new EditorGUI.ChangeCheckScope())
+        {
+            GUILayout.Label("Selection Group Properties", EditorStyles.largeLabel);
+            m_group.Name = EditorGUILayout.TextField("Group Name", m_group.Name);
+            m_group.Color = EditorGUILayout.ColorField("Color", m_group.Color);
+            EditorGUILayout.LabelField("GameObject Query");
+            var q = m_group.Query;
+            var newQuery = EditorGUILayout.TextField(m_group.Query);
+            refreshQuery = refreshQuery || (q != newQuery);
+            
+            if (refreshQuery)
+            {
+                {
+                    var obj = m_group as Object;
+                    //[TODO-sin:2021-12-20] Remove in version 0.7.0 
+                    // if(obj == null)
+                    //     Undo.RegisterCompleteObjectUndo(SelectionGroupPersistenceManager.Instance, "Query change");
+                    // else
+                    //    Undo.RegisterCompleteObjectUndo(obj, "Query change");
+                    
+                    Undo.RegisterCompleteObjectUndo(obj, "Query change");
+                    
+                }
+                m_group.Query = newQuery;
+                var code = GoQL.Parser.Parse(m_group.Query, out GoQL.ParseResult parseResult);
+                if (parseResult == GoQL.ParseResult.OK)
+                {
+                    executor.Code = m_group.Query;
+                    var objects = executor.Execute();
+                    message = $"{objects.Length} results.";
+                    m_group.SetMembers(objects);
+                    parentWindow.Repaint();
+                }
+                else
+                {
+                    message = parseResult.ToString();
+                }
+                refreshQuery = false;
+            }
+            if (message != string.Empty)
+            {
+                GUILayout.Space(5);
+                EditorGUILayout.HelpBox(message, MessageType.Info);
+            }
+            GUILayout.Space(5);
+            
+            //[TODO-sin:2021-12-20] Remove in version 0.7.0 
+            // SelectionGroupDataLocation scope = @group.Scope;
+            // scope = (SelectionGroupDataLocation) EditorGUILayout.EnumPopup(@group.Scope);
+            // if (scope != @group.Scope)
+            // {
+            //     SelectionGroupManager.ChangeGroupScope(group, scope);
+            //     Close();
+            // }
+            
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Enabled Toolbar Buttons", EditorStyles.largeLabel);
+            foreach (var i in TypeCache.GetMethodsWithAttribute<SelectionGroupToolAttribute>())
+            {
+                var attr = i.GetCustomAttribute<SelectionGroupToolAttribute>();
+                var enabled = m_group.EnabledTools.Contains(attr.toolId);
+                var content = EditorGUIUtility.IconContent(attr.icon);
+                var _enabled = EditorGUILayout.ToggleLeft(content, enabled, "button");
+                if (enabled && !_enabled)
+                {
+                    m_group.EnabledTools.Remove(attr.toolId);
+                }
+                if (!enabled && _enabled)
+                {
+                    m_group.EnabledTools.Add(attr.toolId);
+                }
+            }
+            GUILayout.EndVertical();
+            
+            showDebug = GUILayout.Toggle(showDebug, "Show Debug Info", "button");
+            if (showDebug)
+            {
+                if (debugInformation == null) debugInformation = new SelectionGroupDebugInformation(m_group);
+                EditorGUILayout.TextArea(debugInformation.text);
+            }
+            else
+            {
+                debugInformation = null;
+            }
+        }
+    }
+
+//----------------------------------------------------------------------------------------------------------------------    
+    
+
+    ISelectionGroup                m_group;
+    
+    GoQL.GoQLExecutor              executor = new GoQL.GoQLExecutor();
+    SelectionGroupEditorWindow     parentWindow;
+    string                         message      = string.Empty;
+    bool                           refreshQuery = true;
+    bool                           showDebug    = false;
+    SelectionGroupDebugInformation debugInformation;
+    
 }
 
 } //end namespace
