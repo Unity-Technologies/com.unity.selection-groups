149633b29101ce061c430c90c0d14226c5b77b9e
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-01-06 10:51:29 +0900

    convention

--- Editor/Scripts/SelectionGroupInspector.cs
+++ Editor/Scripts/SelectionGroupInspector.cs
@@ -1,22 +1,22 @@
 using Unity.SelectionGroups.Runtime;
 using UnityEditor;
 
-namespace Unity.SelectionGroupsEditor
-{
-    [CustomEditor(typeof(Unity.SelectionGroups.Runtime.SelectionGroup))]
-    internal class SelectionGroupInspector : Editor
+namespace Unity.SelectionGroupsEditor {
+
+[CustomEditor(typeof(Unity.SelectionGroups.Runtime.SelectionGroup))]
+internal class SelectionGroupInspector : Editor {
+    public override void OnInspectorGUI()
     {
-        public override void OnInspectorGUI()
-        {
-            var group = (ISelectionGroup) target;
-            var scope = group.Scope;
-            var query = group.Query;
-            base.OnInspectorGUI();
-            if(group.Query != query)
-                SelectionGroupManager.ExecuteQuery(group);
-            //[TODO-sin:2021-12-20] Remove in version 0.7.0 
-            // if(group.Scope != scope) 
-            //     SelectionGroupManager.ChangeGroupScope(group, group.Scope);
-        }
+        var group = (ISelectionGroup) target;
+        var scope = group.Scope;
+        var query = group.Query;
+        base.OnInspectorGUI();
+        if(group.Query != query)
+            SelectionGroupManager.ExecuteQuery(group);
+        //[TODO-sin:2021-12-20] Remove in version 0.7.0 
+        // if(group.Scope != scope) 
+        //     SelectionGroupManager.ChangeGroupScope(group, group.Scope);
     }
 }
+
+} //end namespace
