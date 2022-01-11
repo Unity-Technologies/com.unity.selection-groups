66ca781148d41c1f7cf6aadb6f0c35b3b6aaa9c4
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-01-11 16:23:50 +0900

    Test with Undo

--- Tests/Runtime/Scripts/SelectionGroupTests.cs
+++ Tests/Runtime/Scripts/SelectionGroupTests.cs
@@ -1,16 +1,23 @@
+using System.Collections;
 using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Unity.SelectionGroups.Tests 
 {
 internal class SelectionGroupTests {
     
     [Test]
-    public void CreateEmptyGroup() {
+    public void CreateEmptyGroupAndUndo() {
         SelectionGroupManager groupManager = GetAndInitGroupManager();
         SelectionGroup        group        = groupManager.CreateSceneSelectionGroup("TestGroup", Color.green);
         Assert.IsNotNull(group);
         Assert.AreEqual(1, groupManager.Groups.Count);
+        
+        Undo.PerformUndo();
+        Assert.AreEqual(0, groupManager.Groups.Count);
+        
     }
     
 //----------------------------------------------------------------------------------------------------------------------
