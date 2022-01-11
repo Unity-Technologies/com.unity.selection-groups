a8b5209261472fc2c92533ee688099b4e4e56ab9
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-01-11 10:28:07 +0900

    Add a test to delete group by API

--- Tests/Runtime/Scripts/SelectionGroupTests.cs
+++ Tests/Runtime/Scripts/SelectionGroupTests.cs
@@ -7,7 +7,7 @@ internal class SelectionGroupTests {
     
     [Test]
     public void CreateEmptyGroup() {
-        SelectionGroupManager groupManager = GetAndInitGroupManager();         
+        SelectionGroupManager groupManager = GetAndInitGroupManager();
         SelectionGroup        group        = groupManager.CreateSceneSelectionGroup("TestGroup", Color.green);
         Assert.IsNotNull(group);
         Assert.AreEqual(1, groupManager.Groups.Count);
@@ -17,7 +17,7 @@ internal class SelectionGroupTests {
     
     [Test]
     public void DeleteGroupComponent() {
-        SelectionGroupManager groupManager = GetAndInitGroupManager();         
+        SelectionGroupManager groupManager = GetAndInitGroupManager();
         SelectionGroup        group        = groupManager.CreateSceneSelectionGroup("TestGroup", Color.green);       
         Object.DestroyImmediate(group);
         Assert.AreEqual(0, groupManager.Groups.Count);
@@ -25,11 +25,20 @@ internal class SelectionGroupTests {
     
     [Test]
     public void DeleteGroupGameObject() {
-        SelectionGroupManager groupManager = GetAndInitGroupManager();         
+        SelectionGroupManager groupManager = GetAndInitGroupManager();
         SelectionGroup        group        = groupManager.CreateSceneSelectionGroup("TestGroup", Color.green);       
         Object.DestroyImmediate(group);
         Assert.AreEqual(0, groupManager.Groups.Count);
     }
+    
+    [Test]
+    public void DeleteGroupByAPI() {
+        SelectionGroupManager groupManager = GetAndInitGroupManager();
+        SelectionGroup        group        = groupManager.CreateSceneSelectionGroup("TestGroup", Color.green);
+        groupManager.DeleteGroup(group);
+        Assert.AreEqual(0, groupManager.Groups.Count);
+    }
+    
     
 //----------------------------------------------------------------------------------------------------------------------
 
