061e4e2124dfed8ee345876d2aa3b7906cd9ad44
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-02-03 20:44:02 +0900

    Revert "add functions in GroupMembersSelection"
    This reverts commit 132bb561493d74ec1ad5699bf44869c7f3fe3226.

--- Runtime/Scripts/GroupMembersSelection.cs
+++ Runtime/Scripts/GroupMembersSelection.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using Object = UnityEngine.Object;
 
 namespace Unity.SelectionGroups
@@ -34,24 +33,6 @@ internal class GroupMembersSelection : IEnumerable<KeyValuePair<ISelectionGroup,
     }
     
 //----------------------------------------------------------------------------------------------------------------------
-
-    [CanBeNull]
-    internal Object GetFirstObject() {
-        foreach (KeyValuePair<ISelectionGroup, OrderedSet<Object>> kv in m_selectedGroupMembers) {
-            foreach (Object member in kv.Value) {
-                return member;
-            }
-        }
-        return null;
-    }
-
-    internal int FindNumUniqueObjects() {
-        HashSet<Object> uniqueDraggedObjects = new HashSet<Object>();
-        foreach (KeyValuePair<ISelectionGroup, OrderedSet<Object>> members in m_selectedGroupMembers) {
-            uniqueDraggedObjects.UnionWith(members.Value);
-        }
-        return uniqueDraggedObjects.Count;
-    }
     
     internal void AddObject(ISelectionGroup group, Object member) {
         if (!m_selectedGroupMembers.ContainsKey(group)) {
