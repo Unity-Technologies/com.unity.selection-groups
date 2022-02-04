0422bccbeb021f7570b44cbfd851103f55c68ce7
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-02-04 11:11:15 +0900

    add constants

---
+++ Editor/Scripts/SelectionGroupEditorConstants.cs
@@ -0,0 +1,23 @@
+using System.IO;
+
+namespace Unity.SelectionGroups.Editor {
+
+internal class SelectionGroupEditorConstants {
+
+    //USS
+    private static readonly  string USS_FOLDER = Path.Combine(SelectionGroupConstants.PACKAGES_PATH,"Editor/StyleSheets");
+    internal static readonly string PROJECT_SETTINGS_STYLE_PATH = Path.Combine(USS_FOLDER,"ProjectSettings_Style");
+    
+    // XML
+    internal static readonly string MAIN_PROJECT_SETTINGS_PATH = ProjSettingsUIPath("ProjectSettings_Main");
+
+    private static readonly string PROJECT_SETTINGS_UIELEMENTS_PATH = Path.Combine(SelectionGroupConstants.PACKAGES_PATH,"Editor/UIElements/ProjectSettings");
+    
+//----------------------------------------------------------------------------------------------------------------------
+    private static string ProjSettingsUIPath(string uiElementRelativePath) {
+        return Path.Combine(PROJECT_SETTINGS_UIELEMENTS_PATH, uiElementRelativePath);
+    }
+
+}
+
+} //end namespace
---
+++ Runtime/Scripts/SelectionGroupConstants.cs
@@ -0,0 +1,7 @@
+namespace Unity.SelectionGroups {
+
+internal static class SelectionGroupConstants {
+    internal const string PACKAGES_PATH = "Packages/com.unity.selection-groups";    
+}
+
+} //end namespace
