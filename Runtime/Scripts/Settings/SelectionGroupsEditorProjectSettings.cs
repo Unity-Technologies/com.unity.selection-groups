64466b08b46c515ddfd967148e7dc3caa69e03df
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-02-08 13:48:46 +0900

    feat: add settings to set the default toolbar buttons for new SelectionGroup (#204)
    * update the settings data (json)
    * update the UI for SelectionGroup project settings
    * apply the settings when creating a new group

--- Runtime/Scripts/Settings/SelectionGroupsEditorProjectSettings.cs
+++ Runtime/Scripts/Settings/SelectionGroupsEditorProjectSettings.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Unity.FilmInternalUtilities;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 
 namespace Unity.SelectionGroups {
@@ -23,10 +25,20 @@ internal class SelectionGroupsEditorProjectSettings : BaseJsonSingleton<Selectio
         m_groupsVisibleInHierarchy = visible; 
     }
 
+    internal bool GetDefaultGroupEditorToolStatus(int toolID) => m_defaultGroupEditorToolStatus[toolID];
+
+    public void EnableDefaultGroupEditorTool(int toolID, bool toolEnabled) {
+        Assert.IsTrue(toolID < (int)SelectionGroupToolType.MAX);
+        m_defaultGroupEditorToolStatus[toolID] = toolEnabled;
+    }
+    
+    
 //----------------------------------------------------------------------------------------------------------------------
     
-    [SerializeField] private bool m_groupsVisibleInHierarchy = true;
-
+    [SerializeField] private bool m_groupsVisibleInHierarchy       = true;
+    
+    [SerializeField] List<bool> m_defaultGroupEditorToolStatus = new List<bool>(new bool[(int) SelectionGroupToolType.MAX]);
+    
 //----------------------------------------------------------------------------------------------------------------------
     private const int LATEST_VERSION = (int) Version.INITIAL; 
     enum Version {
