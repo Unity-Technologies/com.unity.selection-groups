804a261101d11bc5e3646b9372d4ad41fdbfe45f
Mech <mechworrior20@gmail.com>
2022-02-11 11:26:16 -0800

    Updated static path to UITK files to reflection changed location

--- Editor/SelectionGroupEditorConstants.cs
+++ Editor/SelectionGroupEditorConstants.cs
@@ -5,8 +5,7 @@ namespace Unity.SelectionGroups.Editor {
 internal class SelectionGroupEditorConstants {
 
     //USS
-    private static readonly  string USS_FOLDER = Path.Combine(SelectionGroupConstants.PACKAGES_PATH,"Editor/StyleSheets");
-    internal static readonly string PROJECT_SETTINGS_STYLE_PATH = Path.Combine(USS_FOLDER,"ProjectSettings_Style");
+    internal static readonly string PROJECT_SETTINGS_STYLE_PATH = ProjSettingsUIPath("ProjectSettings_Style");
     
     // XML
     internal static readonly string MAIN_PROJECT_SETTINGS_PATH = ProjSettingsUIPath("ProjectSettings_Main");
@@ -14,8 +13,8 @@ internal class SelectionGroupEditorConstants {
     
 //----------------------------------------------------------------------------------------------------------------------
     private static string ProjSettingsUIPath(string uiElementRelativePath) {
-        const string UIELEMENTS_PATH = "Editor/UIElements/ProjectSettings";
-        return Path.Combine(SelectionGroupConstants.PACKAGES_PATH,UIELEMENTS_PATH,uiElementRelativePath);
+        const string UIELEMENTS_PATH = "Editor/ProjectSettings";
+        return Path.Combine(SelectionGroupConstants.PACKAGES_PATH,UIELEMENTS_PATH, uiElementRelativePath);
         
     }
 
