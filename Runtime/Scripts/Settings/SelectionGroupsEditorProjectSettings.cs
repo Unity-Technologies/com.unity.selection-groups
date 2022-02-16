86628a168eddc0833964f379faf56ab704e59da4
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-02-16 20:21:41 +0900

    restore

--- Runtime/Scripts/Settings/SelectionGroupsEditorProjectSettings.cs
+++ Runtime/Scripts/Settings/SelectionGroupsEditorProjectSettings.cs
@@ -15,6 +15,18 @@ internal class SelectionGroupsEditorProjectSettings : BaseJsonSingleton<Selectio
 
     protected override void UpgradeToLatestVersionV(int prevVersion, int curVersion) {
         
+        if (prevVersion < (int) SGProjectSettingsVersion.EDITOR_STATE_0_7_2) {
+#pragma warning disable 612 //obsolete
+            if (null != m_defaultGroupEditorToolStatus) {
+                int numStates = m_defaultGroupEditorToolStatus.Count;
+                for (int i = 0; i < numStates; ++i) {
+                    m_defaultGroupEditorToolStates[i] = m_defaultGroupEditorToolStatus[i];
+                }
+                m_defaultGroupEditorToolStatus = null;
+#pragma warning restore 612
+            }
+        }
+        
     }
     
 //----------------------------------------------------------------------------------------------------------------------
@@ -40,12 +52,17 @@ internal class SelectionGroupsEditorProjectSettings : BaseJsonSingleton<Selectio
     
     [SerializeField] private bool m_groupsVisibleInHierarchy       = true;
     
+    [Obsolete]
+    [SerializeField] List<bool> m_defaultGroupEditorToolStatus = null;
+    
     [SerializeField] EditorToolStates m_defaultGroupEditorToolStates = new EditorToolStates(); 
     
 //----------------------------------------------------------------------------------------------------------------------
-    private const int LATEST_VERSION = (int) Version.INITIAL; 
-    enum Version {
+    private const int LATEST_VERSION = (int) SGProjectSettingsVersion.INITIAL; 
+    enum SGProjectSettingsVersion {
         INITIAL = 1,
+        EDITOR_STATE_0_7_2, //The data structure of m_defaultGroupEditorToolStates was changed
+        
     };
 
 
