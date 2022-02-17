82594f2c9d6cafbfb0aeb265e6505e23c93cad68
Mech <mechworrior20@gmail.com>
2022-02-16 21:50:16 -0800

    Update SelectionGroupsEditorProjectSettings.cs
    Updated class indentation.
    
    Moved enum declaration to top of class.
    Moved fields to top of class for consistancy.
    
    Renamed enum values to follow C# conventions.
    
    Renamed const to follow Unity's naming conventions.
    
    Renamed fields to follow Unity's naming conventions.
    
    Made field accessibility explicit for consistency.
    
    Updated curly brace indentation for consistancy.

--- Runtime/Scripts/Settings/SelectionGroupsEditorProjectSettings.cs
+++ Runtime/Scripts/Settings/SelectionGroupsEditorProjectSettings.cs
@@ -3,69 +3,69 @@ using System.Collections.Generic;
 using Unity.FilmInternalUtilities;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.Serialization;
 
 
-namespace Unity.SelectionGroups {
-
-[Json("ProjectSettings/SelectionGroupsSettings.asset")]
-[Serializable]
-internal class SelectionGroupsEditorProjectSettings : BaseJsonSingleton<SelectionGroupsEditorProjectSettings> {
-
-    protected override int GetLatestVersionV() => LATEST_VERSION;
-
-    protected override void UpgradeToLatestVersionV(int prevVersion, int curVersion) {
+namespace Unity.SelectionGroups 
+{
+    [Json("ProjectSettings/SelectionGroupsSettings.asset")]
+    [Serializable]
+    internal class SelectionGroupsEditorProjectSettings : BaseJsonSingleton<SelectionGroupsEditorProjectSettings> 
+    {
+        private enum SGProjectSettingsVersion 
+        {
+            Initial = 1,
+            EditorState_0_7_2, //The data structure of m_defaultGroupEditorToolStates was changed
+        };
         
-        if (prevVersion < (int) SGProjectSettingsVersion.EDITOR_STATE_0_7_2) {
-#pragma warning disable 612 //obsolete
-            if (null != m_defaultGroupEditorToolStatus) {
-                int numStates = m_defaultGroupEditorToolStatus.Count;
-                for (int i = 0; i < numStates; ++i) {
-                    m_defaultGroupEditorToolStates[i] = m_defaultGroupEditorToolStatus[i];
+        private const int kLatestVersion = (int) SGProjectSettingsVersion.EditorState_0_7_2; 
+        
+        [FormerlySerializedAs("m_groupsVisibleInHierarchy")] 
+        [SerializeField] private bool m_GroupsVisibleInHierarchy = true;
+        
+        [Obsolete]
+        [SerializeField] private List<bool> m_defaultGroupEditorToolStatus = null;
+        
+        [FormerlySerializedAs("m_defaultGroupEditorToolStates")] 
+        [SerializeField] private EditorToolStates m_DefaultGroupEditorToolStates = new EditorToolStates(); 
+        
+        protected override int GetLatestVersionV() => kLatestVersion;
+
+        protected override void UpgradeToLatestVersionV(int prevVersion, int curVersion) 
+        {
+            if (prevVersion < (int) SGProjectSettingsVersion.EditorState_0_7_2) 
+            {
+    #pragma warning disable 612 //obsolete
+                if (null != m_defaultGroupEditorToolStatus) 
+                {
+                    int numStates = m_defaultGroupEditorToolStatus.Count;
+                    for (int i = 0; i < numStates; ++i) 
+                    {
+                        m_DefaultGroupEditorToolStates[i] = m_defaultGroupEditorToolStatus[i];
+                    }
+                    m_defaultGroupEditorToolStatus = null;
+    #pragma warning restore 612
                 }
-                m_defaultGroupEditorToolStatus = null;
-#pragma warning restore 612
             }
         }
         
-    }
-    
-//----------------------------------------------------------------------------------------------------------------------
-
-    internal bool AreGroupsVisibleInHierarchy() => m_groupsVisibleInHierarchy; 
-    
-    internal void ShowGroupsInHierarchy(bool visible) {
-        m_groupsVisibleInHierarchy = visible; 
-    }
-
-    internal bool GetDefaultGroupEditorToolState(int toolID) {
-        if (m_defaultGroupEditorToolStates.TryGetValue(toolID, out bool status))
-            return status;
-        return false;
-    }
-
-    public void EnableDefaultGroupEditorTool(int toolID, bool toolEnabled) {
-        m_defaultGroupEditorToolStates[toolID] = toolEnabled;
-    }
-    
-    
-//----------------------------------------------------------------------------------------------------------------------
-    
-    [SerializeField] private bool m_groupsVisibleInHierarchy       = true;
-    
-    [Obsolete]
-    [SerializeField] List<bool> m_defaultGroupEditorToolStatus = null;
-    
-    [SerializeField] EditorToolStates m_defaultGroupEditorToolStates = new EditorToolStates(); 
-    
-//----------------------------------------------------------------------------------------------------------------------
-    private const int LATEST_VERSION = (int) SGProjectSettingsVersion.EDITOR_STATE_0_7_2; 
-    enum SGProjectSettingsVersion {
-        INITIAL = 1,
-        EDITOR_STATE_0_7_2, //The data structure of m_defaultGroupEditorToolStates was changed
+        internal bool AreGroupsVisibleInHierarchy() => m_GroupsVisibleInHierarchy; 
         
-    };
+        internal void ShowGroupsInHierarchy(bool visible) 
+        {
+            m_GroupsVisibleInHierarchy = visible; 
+        }
 
+        internal bool GetDefaultGroupEditorToolState(int toolID) 
+        {
+            if (m_DefaultGroupEditorToolStates.TryGetValue(toolID, out bool status))
+                return status;
+            return false;
+        }
 
+        public void EnableDefaultGroupEditorTool(int toolID, bool toolEnabled) 
+        {
+            m_DefaultGroupEditorToolStates[toolID] = toolEnabled;
+        }
+    }
 }
-
-} //end namespace
