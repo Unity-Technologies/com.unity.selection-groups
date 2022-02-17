e43984dd02906b0922e92f44759d8e48e97e66d4
Mech <mechworrior20@gmail.com>
2022-02-16 22:00:47 -0800

    Update SelectionGroupToolAttributeCache.cs
    Changed class indentation.
    
    Moved fields to top of class for consistency.
    
    Renamed fields to match Unity's naming convention.
    
    Made accesibilies explicit.
    
    Removed separator commends "//------------.---"
    
    Moved curly braces to be on their own line for consitancy.

--- Editor/Scripts/Tools/SelectionGroupToolAttributeCache.cs
+++ Editor/Scripts/Tools/SelectionGroupToolAttributeCache.cs
@@ -5,39 +5,35 @@ using UnityEditor;
 
 namespace Unity.SelectionGroups.Editor
 {
-internal static class SelectionGroupToolAttributeCache {
+    internal static class SelectionGroupToolAttributeCache 
+    {
+        private static readonly TypeCache.MethodCollection m_ToolMethods = TypeCache.GetMethodsWithAttribute<SelectionGroupToolAttribute>();        
 
-    [InitializeOnLoadMethod]
-    static void SelectionGroupToolAttributeCache_OnLoad() {
+        private static readonly Dictionary<int, SelectionGroupToolAttribute> m_ToolAttributeMap = new Dictionary<int, SelectionGroupToolAttribute>();
+        private static readonly Dictionary<int, MethodInfo> m_ToolMethodInfoMap = new Dictionary<int, MethodInfo>();
+
+        [InitializeOnLoadMethod]
+        private static void SelectionGroupToolAttributeCache_OnLoad() 
+        {
+            m_ToolAttributeMap.Clear();
+
+            foreach (MethodInfo methodInfo in m_ToolMethods) 
+            {
+                SelectionGroupToolAttribute attr = methodInfo.GetCustomAttribute<SelectionGroupToolAttribute>();
+                m_ToolMethodInfoMap[attr.toolId] = methodInfo;
+                m_ToolAttributeMap[attr.toolId]  = attr;            
+            }
+        }
         
-        m_toolAttributeMap.Clear();
+        internal static bool TryGetAttribute(int id, out SelectionGroupToolAttribute attribute) 
+        {
+            return m_ToolAttributeMap.TryGetValue(id, out attribute);            
+        }
 
-        foreach (MethodInfo methodInfo in m_toolMethods) {
-            SelectionGroupToolAttribute attr = methodInfo.GetCustomAttribute<SelectionGroupToolAttribute>();
-            m_toolMethodInfoMap[attr.toolId] = methodInfo;
-            m_toolAttributeMap[attr.toolId]  = attr;            
+        internal static bool TryGetMethodInfo(int id, out MethodInfo methodInfo) 
+        {
+            return m_ToolMethodInfoMap.TryGetValue(id, out methodInfo);            
         }
     }
-    
-//----------------------------------------------------------------------------------------------------------------------
-
-    internal static bool TryGetAttribute(int id, out SelectionGroupToolAttribute attribute) {
-        return m_toolAttributeMap.TryGetValue(id, out attribute);            
-    }
-
-    internal static bool TryGetMethodInfo(int id, out MethodInfo methodInfo) {
-        return m_toolMethodInfoMap.TryGetValue(id, out methodInfo);            
-    }
-    
-
-//----------------------------------------------------------------------------------------------------------------------
-    
-    private static readonly TypeCache.MethodCollection m_toolMethods = TypeCache.GetMethodsWithAttribute<SelectionGroupToolAttribute>();        
-
-    private static readonly Dictionary<int, SelectionGroupToolAttribute> m_toolAttributeMap = new Dictionary<int, SelectionGroupToolAttribute>();
-    private static readonly Dictionary<int, MethodInfo> m_toolMethodInfoMap = new Dictionary<int, MethodInfo>();
-    
 
 }
-
-} //end namespace
