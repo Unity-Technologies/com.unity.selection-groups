85f173a77b2405bf45fb7479fa75883d8b27f7cb
Mech <mechworrior20@gmail.com>
2022-02-16 21:43:08 -0800

    Update SelectionGroupUtility.cs
    Updated class indentation.
    
    Changed RegisterUndo accessibility explicit for consistency.
    
    Moved curly braces to their own line for consistency.

--- Runtime/Scripts/Utilities/SelectionGroupUtility.cs
+++ Runtime/Scripts/Utilities/SelectionGroupUtility.cs
@@ -7,46 +7,44 @@ using UnityEditor;
 
 namespace Unity.SelectionGroups
 {
-
-internal static class SelectionGroupUtility
-{
-    //move prevMembersSelection to targetGroup, and return the new members selection
-    internal static GroupMembersSelection MoveMembersSelectionToGroup(GroupMembersSelection prevMembersSelection, 
-        SelectionGroup targetGroup) 
+    internal static class SelectionGroupUtility
     {
-        GroupMembersSelection newMembersSelection = new GroupMembersSelection(prevMembersSelection);
-        RegisterUndo(targetGroup, "Move Members");
-            
-        foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in prevMembersSelection) {
-            SelectionGroup prevGroup = kv.Key;
-            if (null == prevGroup)
-                continue;
+        //move prevMembersSelection to targetGroup, and return the new members selection
+        internal static GroupMembersSelection MoveMembersSelectionToGroup(GroupMembersSelection prevMembersSelection, 
+            SelectionGroup targetGroup) 
+        {
+            GroupMembersSelection newMembersSelection = new GroupMembersSelection(prevMembersSelection);
+            RegisterUndo(targetGroup, "Move Members");
                 
-            if (targetGroup == prevGroup)
-                continue;
+            foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in prevMembersSelection) 
+            {
+                SelectionGroup prevGroup = kv.Key;
+                if (null == prevGroup)
+                    continue;
+                    
+                if (targetGroup == prevGroup)
+                    continue;
 
-            RegisterUndo(prevGroup, "Move Members");
-                                            
-            foreach (Object obj in kv.Value) {
-                newMembersSelection.AddObject(targetGroup, obj);
-                targetGroup.Add(obj);
-                prevGroup.Remove(obj);
-                newMembersSelection.RemoveObject(prevGroup, obj);
+                RegisterUndo(prevGroup, "Move Members");
+                                                
+                foreach (Object obj in kv.Value) 
+                {
+                    newMembersSelection.AddObject(targetGroup, obj);
+                    targetGroup.Add(obj);
+                    prevGroup.Remove(obj);
+                    newMembersSelection.RemoveObject(prevGroup, obj);
+                }
             }
+
+            return newMembersSelection;
         }
-
-        return newMembersSelection;
+        
+        private static void RegisterUndo(SelectionGroup group, string msg)
+        {
+    #if UNITY_EDITOR
+            Undo.RegisterCompleteObjectUndo(group, msg);
+            EditorUtility.SetDirty(group);
+    #endif
+        }
     }
-    
-    
-    static void RegisterUndo(SelectionGroup group, string msg)
-    {
-#if UNITY_EDITOR
-        Undo.RegisterCompleteObjectUndo(group, msg);
-        EditorUtility.SetDirty(group);
-#endif
-    }
-    
-
 }
-} //end namespace
