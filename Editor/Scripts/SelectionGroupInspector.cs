9f26ae7618c3faf156f9ef94cd82754fc1a17e33
Mech <mechworrior20@gmail.com>
2022-02-16 21:34:11 -0800

    Update SelectionGroupInspector.cs
    Updated class indentation.
    
    Moved fields to top of class.
    
    Updated field naming to be consistent with Unity's naming style.
    
    Moved OnEnable() method to top for consistency with other scripts and so the methods are in call order which can make the code easier to read.
    
    Updated curly braces to be on their own line for consistancy.

--- Editor/Scripts/SelectionGroupInspector.cs
+++ Editor/Scripts/SelectionGroupInspector.cs
@@ -6,90 +6,94 @@ using UnityEditor;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
     
-namespace Unity.SelectionGroups.Editor {
-
-[CustomEditor(typeof(SelectionGroups.SelectionGroup))]
-internal class SelectionGroupInspector : UnityEditor.Editor {
-    public override void OnInspectorGUI() {
-        serializedObject.Update();
-
-        bool repaintWindow = EditorGUIDrawerUtility.DrawUndoableGUI(m_group.gameObject, "Group Name",
-            () => EditorGUILayout.TextField("Group Name", m_group.groupName),
-            (string groupName) => { m_group.groupName = groupName; }
-        );
+namespace Unity.SelectionGroups.Editor 
+{
+    [CustomEditor(typeof(SelectionGroups.SelectionGroup))]
+    internal class SelectionGroupInspector : UnityEditor.Editor 
+    {
+        private SelectionGroup m_Group;
+        private bool m_ShowDebug  = false;
+        private SelectionGroupDebugInformation m_DebugInformation;
         
-        repaintWindow |= EditorGUIDrawerUtility.DrawUndoableGUI(m_group, "Group Color",
-            () => EditorGUILayout.ColorField("Color", m_group.color),
-            (Color groupColor) => { m_group.color = groupColor; }
-        );
-
-        repaintWindow |= EditorGUIDrawerUtility.DrawUndoableGUI(m_group, "Group Query",
-            () => EditorGUILayout.TextField("Group Query", m_group.query),
-            (string query) => {
-                {
-                    Undo.RegisterCompleteObjectUndo(m_group, "Query change");
-                }
-                m_group.SetQuery(query);
-            }
-        );
-
-        //Query results
-        if (m_group.IsAutoFilled()) {
-            GoQL.ParseResult parseResult = m_group.GetLastQueryParseResult();
-            
-            string message = (parseResult == GoQL.ParseResult.OK) 
-                ? $"{m_group.Members.Count} results." 
-                : parseResult.ToString();
-
-            GUILayout.Space(5);
-            EditorGUILayout.HelpBox(message, MessageType.Info);
+        private void OnEnable() 
+        {
+            m_Group = target as SelectionGroup;
         }
-                
-        GUILayout.BeginVertical("box");
-        GUILayout.Label("Enabled Toolbar Buttons", EditorStyles.largeLabel);
-        foreach (MethodInfo i in TypeCache.GetMethodsWithAttribute<SelectionGroupToolAttribute>()) {
-            SelectionGroupToolAttribute attr = i.GetCustomAttribute<SelectionGroupToolAttribute>();            
-            repaintWindow |= EditorGUIDrawerUtility.DrawUndoableGUI(m_group, "Group Toolbar",
-                guiFunc: () => {
-                    bool       isEnabledPrev = m_group.GetEditorToolState(attr.toolId);
-                    GUIContent content       = EditorGUIUtility.IconContent(attr.icon);
-                    return EditorGUILayout.ToggleLeft(content, isEnabledPrev, "button");
-                },
-                updateFunc: (bool toolEnabled) => {
-                    m_group.EnableEditorTool(attr.toolId, toolEnabled);
+        
+        public override void OnInspectorGUI() 
+        {
+            serializedObject.Update();
+
+            bool repaintWindow = EditorGUIDrawerUtility.DrawUndoableGUI(m_Group.gameObject, "Group Name",
+                () => EditorGUILayout.TextField("Group Name", m_Group.groupName),
+                (string groupName) => { m_Group.groupName = groupName; }
+            );
+            
+            repaintWindow |= EditorGUIDrawerUtility.DrawUndoableGUI(m_Group, "Group Color",
+                () => EditorGUILayout.ColorField("Color", m_Group.color),
+                (Color groupColor) => { m_Group.color = groupColor; }
+            );
+
+            repaintWindow |= EditorGUIDrawerUtility.DrawUndoableGUI(m_Group, "Group Query",
+                () => EditorGUILayout.TextField("Group Query", m_Group.query),
+                (string query) => 
+                {
+                    {
+                        Undo.RegisterCompleteObjectUndo(m_Group, "Query change");
+                    }
+                    m_Group.SetQuery(query);
                 }
             );
+
+            //Query results
+            if (m_Group.IsAutoFilled()) 
+            {
+                GoQL.ParseResult parseResult = m_Group.GetLastQueryParseResult();
+                
+                string message = (parseResult == GoQL.ParseResult.OK) 
+                    ? $"{m_Group.Members.Count} results." 
+                    : parseResult.ToString();
+
+                GUILayout.Space(5);
+                EditorGUILayout.HelpBox(message, MessageType.Info);
+            }
+                    
+            GUILayout.BeginVertical("box");
+            GUILayout.Label("Enabled Toolbar Buttons", EditorStyles.largeLabel);
+            foreach (MethodInfo i in TypeCache.GetMethodsWithAttribute<SelectionGroupToolAttribute>()) 
+            {
+                SelectionGroupToolAttribute attr = i.GetCustomAttribute<SelectionGroupToolAttribute>();            
+                repaintWindow |= EditorGUIDrawerUtility.DrawUndoableGUI(m_Group, "Group Toolbar",
+                    guiFunc: () => 
+                    {
+                        bool       isEnabledPrev = m_Group.GetEditorToolState(attr.toolId);
+                        GUIContent content       = EditorGUIUtility.IconContent(attr.icon);
+                        return EditorGUILayout.ToggleLeft(content, isEnabledPrev, "button");
+                    },
+                    updateFunc: (bool toolEnabled) => 
+                    {
+                        m_Group.EnableEditorTool(attr.toolId, toolEnabled);
+                    }
+                );
+            }
+            GUILayout.EndVertical();
+            
+            m_ShowDebug = GUILayout.Toggle(m_ShowDebug, "Show Debug Info", "button");
+            if (m_ShowDebug) 
+            {
+                if (m_DebugInformation == null) m_DebugInformation = new SelectionGroupDebugInformation(m_Group);
+                EditorGUILayout.TextArea(m_DebugInformation.text);
+            } 
+            else 
+            {
+                m_DebugInformation = null;
+            }
+            serializedObject.ApplyModifiedProperties();
+
+            if (!repaintWindow)
+                return;
+            
+            SelectionGroupEditorWindow.TryRepaint();
         }
-        GUILayout.EndVertical();
-        
-        m_showDebug = GUILayout.Toggle(m_showDebug, "Show Debug Info", "button");
-        if (m_showDebug) {
-            if (m_debugInformation == null) m_debugInformation = new SelectionGroupDebugInformation(m_group);
-            EditorGUILayout.TextArea(m_debugInformation.text);
-        } else {
-            m_debugInformation = null;
-        }
-        serializedObject.ApplyModifiedProperties();
-
-        if (!repaintWindow)
-            return;
-        
-        SelectionGroupEditorWindow.TryRepaint();
-
     }
-    
-//----------------------------------------------------------------------------------------------------------------------    
-    private void OnEnable() {
-        m_group = target as SelectionGroup;
-    }
-   
-//----------------------------------------------------------------------------------------------------------------------    
-    
-
-    SelectionGroup m_group;
-    
-    bool                           m_showDebug  = false;
-    SelectionGroupDebugInformation m_debugInformation;
 }
-
-} //end namespace
