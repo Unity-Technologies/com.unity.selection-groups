871eff9d0a1d1178a24635bd59feedf6b9525e50
Mech <mechworrior20@gmail.com>
2022-02-16 21:23:15 -0800

    Update GroupMembersSelection.cs
    Changed class indentation
    Moved selection group members field to top of class.
    
    Renamed field to match Unity's style.
    
    Moved curly brackets to own line for consistency.

--- Runtime/Scripts/GroupMembersSelection.cs
+++ Runtime/Scripts/GroupMembersSelection.cs
@@ -4,120 +4,134 @@ using Object = UnityEngine.Object;
 
 namespace Unity.SelectionGroups
 {
+    //A class to hold selected members from selected groups
+    internal class GroupMembersSelection : IEnumerable<KeyValuePair<SelectionGroup, OrderedSet<Object>>>
+    {
+        private readonly Dictionary<SelectionGroup, OrderedSet<Object>> m_SelectedGroupMembers 
+            = new Dictionary<SelectionGroup, OrderedSet<Object>>();
+        
+        internal GroupMembersSelection() { }
 
-//A class to hold selected members from selected groups
-internal class GroupMembersSelection : IEnumerable<KeyValuePair<SelectionGroup, OrderedSet<Object>>>
-{
+        internal GroupMembersSelection(GroupMembersSelection other) 
+        {
+            foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in other) 
+            {
+                OrderedSet<Object> collection = new OrderedSet<Object>() { };
+                foreach (Object member in kv.Value) 
+                {
+                    collection.Add(member);
+                }
 
-    internal GroupMembersSelection() { }
+                m_SelectedGroupMembers[kv.Key] = collection;
+            }
+        }
+    //----------------------------------------------------------------------------------------------------------------------
+        
+        public IEnumerator<KeyValuePair<SelectionGroup, OrderedSet<Object>>> GetEnumerator() 
+        {
+            return m_SelectedGroupMembers.GetEnumerator();
+        }
 
-    internal GroupMembersSelection(GroupMembersSelection other) {
-
-        foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in other) {
-            OrderedSet<Object> collection = new OrderedSet<Object>() { };
-            foreach (Object member in kv.Value) {
-                collection.Add(member);
+        IEnumerator IEnumerable.GetEnumerator() 
+        {
+            return this.GetEnumerator();
+        }
+        
+    //----------------------------------------------------------------------------------------------------------------------
+        
+        internal void AddObject(SelectionGroup group, Object member) 
+        {
+            if (!m_SelectedGroupMembers.ContainsKey(group)) 
+            {
+                m_SelectedGroupMembers.Add(group, new OrderedSet<Object>(){member});
+                return;
             }
 
-            m_selectedGroupMembers[kv.Key] = collection;
-        }
-    }
-//----------------------------------------------------------------------------------------------------------------------
-    
-    public IEnumerator<KeyValuePair<SelectionGroup, OrderedSet<Object>>> GetEnumerator() {
-        return m_selectedGroupMembers.GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator() {
-        return this.GetEnumerator();
-    }
-    
-//----------------------------------------------------------------------------------------------------------------------
-    
-    internal void AddObject(SelectionGroup group, Object member) {
-        if (!m_selectedGroupMembers.ContainsKey(group)) {
-            m_selectedGroupMembers.Add(group, new OrderedSet<Object>(){member});
-            return;
+            m_SelectedGroupMembers[group].Add(member);
         }
 
-        m_selectedGroupMembers[group].Add(member);
-    }
-
-    internal void AddGroupMembers(SelectionGroup group) {
-        AddObjects(group, group.Members);
-    }
-    
-    
-    internal void Add(GroupMembersSelection otherSelection) {
-        foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in otherSelection) {
-            SelectionGroup group = kv.Key;
-            AddObjects(group, kv.Value);
-        }
-    }
-    
-
-    private void AddObjects(SelectionGroup group, IEnumerable<Object> objects) {
-        
-        OrderedSet<Object> collection = null;
-        if (!m_selectedGroupMembers.ContainsKey(group)) {
-            collection = new OrderedSet<Object>() { };
-            m_selectedGroupMembers.Add(group, collection);
-        } else {
-            collection = m_selectedGroupMembers[group];
+        internal void AddGroupMembers(SelectionGroup group) 
+        {
+            AddObjects(group, group.Members);
         }
 
-        foreach (Object m in objects) {
-            collection.Add(m);
+        internal void Add(GroupMembersSelection otherSelection) 
+        {
+            foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in otherSelection) 
+            {
+                SelectionGroup group = kv.Key;
+                AddObjects(group, kv.Value);
+            }
         }
         
-    }
-    
+        private void AddObjects(SelectionGroup group, IEnumerable<Object> objects) 
+        {
+            
+            OrderedSet<Object> collection = null;
+            if (!m_SelectedGroupMembers.ContainsKey(group)) 
+            {
+                collection = new OrderedSet<Object>() { };
+                m_SelectedGroupMembers.Add(group, collection);
+            } else 
+            {
+                collection = m_SelectedGroupMembers[group];
+            }
 
-    internal void RemoveObject(SelectionGroup group, Object member) {
-        if (!m_selectedGroupMembers.ContainsKey(group)) {
-            return;
+            foreach (Object m in objects) {
+                collection.Add(m);
+            }
+            
         }
 
-        m_selectedGroupMembers[group].Remove(member);
-    }
+        internal void RemoveObject(SelectionGroup group, Object member) 
+        {
+            if (!m_SelectedGroupMembers.ContainsKey(group))
+            {
+                return;
+            }
 
-    internal void RemoveGroup(SelectionGroup group) {
-        if (!m_selectedGroupMembers.ContainsKey(group)) {
-            return;
+            m_SelectedGroupMembers[group].Remove(member);
         }
-        m_selectedGroupMembers.Remove(group);
-    }
-    
-    internal bool Contains(SelectionGroup group, Object member) {
-        if (!m_selectedGroupMembers.ContainsKey(group))
-            return false;
 
-        return (m_selectedGroupMembers[group].Contains(member));
-    }
+        internal void RemoveGroup(SelectionGroup group) 
+        {
+            if (!m_SelectedGroupMembers.ContainsKey(group)) 
+            {
+                return;
+            }
+            m_SelectedGroupMembers.Remove(group);
+        }
         
-    internal void Clear() {
-        m_selectedGroupMembers.Clear();
-    }
+        internal bool Contains(SelectionGroup group, Object member) 
+        {
+            if (!m_SelectedGroupMembers.ContainsKey(group))
+                return false;
 
-    internal Object[] ConvertMembersToArray() {
-        HashSet<Object> set = ConvertMembersToSet();
-        Object[] arr = new Object[set.Count];
-        set.CopyTo(arr);
-        return arr;
-    }
-
-    internal HashSet<Object> ConvertMembersToSet() {
-        HashSet<Object> set = new HashSet<Object>();
-        foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in m_selectedGroupMembers) {
-            set.UnionWith(kv.Value);
+            return (m_SelectedGroupMembers[group].Contains(member));
+        }
+            
+        internal void Clear() 
+        {
+            m_SelectedGroupMembers.Clear();
         }
 
-        return set;
+        internal Object[] ConvertMembersToArray() 
+        {
+            HashSet<Object> set = ConvertMembersToSet();
+            Object[] arr = new Object[set.Count];
+            set.CopyTo(arr);
+            return arr;
+        }
+
+        internal HashSet<Object> ConvertMembersToSet() 
+        {
+            HashSet<Object> set = new HashSet<Object>();
+            foreach (KeyValuePair<SelectionGroup, OrderedSet<Object>> kv in m_SelectedGroupMembers)
+            {
+                set.UnionWith(kv.Value);
+            }
+
+            return set;
+        }
     }
-    
-//----------------------------------------------------------------------------------------------------------------------    
-    readonly Dictionary<SelectionGroup, OrderedSet<Object>> m_selectedGroupMembers 
-        = new Dictionary<SelectionGroup, OrderedSet<Object>>();
-    
-}
 } //end namespace
