37e04caa098701e45bb1ef9da51a487b5c6a617c
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-04-14 19:26:24 +0900

    chore: fix the script to sync package info (#251)

--- Scripts~/sync_package_info.sh
+++ Scripts~/sync_package_info.sh
@@ -1,5 +1,5 @@
 packageName="com.unity.selection-groups"
 
-cp package.json "SelectionGroups~\\Packages\\${packageName}"
-cp CHANGELOG.md "SelectionGroups~\\Packages\\${packageName}"    
+cp package.json "SelectionGroups~/Packages/${packageName}"
+cp CHANGELOG.md "SelectionGroups~/Packages/${packageName}"    
 
