e6399f00afe57c5ce64343b17ff5b77e6fd7a422
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2022-06-27 16:04:48 +0900

    Revert "remove SelectionGroupDataLocation"
    This reverts commit f4c661fb799d1bcd1a75dc72ab329cdbd5fa5e8d.

---
+++ Runtime/Scripts/SelectionGroupDataLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace Unity.SelectionGroups
+{
+    //[TODO-sin: 2022-4-7] Remove this starting from 0.8.0-preview
+    /// <summary>
+    /// Indicates where the SelectionGroup data is stored
+    /// </summary>
+    public enum SelectionGroupDataLocation
+    {
+        /// <summary>
+        /// Stored in a scene (default)
+        /// </summary>
+        Scene = 1,
+    }
+}
