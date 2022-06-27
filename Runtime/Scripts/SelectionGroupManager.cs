95a00330ba239c2ff9e85cdf028dc99298d66404
Sindharta Tanuwijaya <sindharta@users.noreply.github.com>
2022-06-27 16:48:44 +0900

    refactor: remove obsolete functions to create SelectionGroup (#267)

--- Runtime/Scripts/SelectionGroupManager.cs
+++ Runtime/Scripts/SelectionGroupManager.cs
@@ -32,36 +32,6 @@ internal class SelectionGroupManager : MonoBehaviourSingleton<SelectionGroupMana
 
     internal IEnumerable<string> GroupNames => m_sceneSelectionGroups.Select(g => g.Name);
 
-
-    [Obsolete]
-    internal SelectionGroup CreateSceneSelectionGroup(string groupName, string query, Color color, IList<Object> members) {
-        SelectionGroup group = CreateSelectionGroupInternal(groupName, color);        
-        group.SetQuery(query);
-
-        if (!group.IsAutoFilled()) {
-            group.AddRange(members);
-        }
-
-        m_sceneSelectionGroups.Add(group);
-        return group;
-    }
-
-    [Obsolete]
-    internal SelectionGroup CreateSceneSelectionGroup(string groupName, Color color) {
-        return CreateSelectionGroup(groupName, color);
-    }
-    
-    [Obsolete]
-    internal SelectionGroup CreateSceneSelectionGroup(string groupName, Color color, string query) {
-        return CreateSelectionGroup(groupName, color, query);
-    }
-
-    [Obsolete]
-    internal SelectionGroup CreateSceneSelectionGroup(string groupName, Color color, IList<Object> members) {
-        return CreateSelectionGroup(groupName, color, members);
-    }
-    
-
     //
     internal SelectionGroup CreateSelectionGroup(string groupName, Color color) {
         SelectionGroup group = CreateSelectionGroupInternal(groupName, color);
